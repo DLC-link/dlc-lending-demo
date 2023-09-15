715f7a8d48e8e65c9028b42e05fa487b4bab2d4f
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-09-15 09:55:35 +0200

    feat: changing env var paths

--- src/networks/networks.js
+++ src/networks/networks.js
@@ -1,46 +1,15 @@
-import { StacksMainnet, StacksTestnet, StacksMocknet } from '@stacks/network';
+import { StacksTestnet } from '@stacks/network';
 
-const mainnet = new StacksMainnet();
 const testnet = new StacksTestnet();
-const mocknet = new StacksMocknet({
-  url: process.env.REACT_APP_STACKS_DEVNET_ADDRESS,
-});
 
-export const EthereumNetworks = {
-  'ethereum:5': {
-    name: 'Goerli Testnet',
-    protocolContractAddress: process.env.REACT_APP_GOERLI_PROTOCOL_CONTRACT_ADDRESS,
-    usdcAddress: process.env.REACT_APP_GOERLI_USDC_ADDRESS,
-    dlcManagerAddress: process.env.REACT_APP_GOERLI_DLC_MANAGER_ADDRESS,
-  },
-  'ethereum:11155111': {
+export const EthereumNetwork = {
     name: 'Sepolia Testnet',
     protocolContractAddress: process.env.REACT_APP_SEPOLIA_PROTOCOL_CONTRACT_ADDRESS,
     usdcAddress: process.env.REACT_APP_SEPOLIA_USDC_ADDRESS,
     dlcManagerAddress: process.env.REACT_APP_SEPOLIA_DLC_MANAGER_ADDRESS,
-  },
-  'ethereum:31337': {
-    name: 'Localhost',
-    protocolContractAddress: process.env.REACT_APP_LOCALHOST_PROTOCOL_CONTRACT_ADDRESS,
-    usdcAddress: process.env.REACT_APP_LOCALHOST_USDC_ADDRESS,
-    dlcManagerAddress: process.env.REACT_APP_LOCALHOST_DLC_MANAGER_ADDRESS,
-  },
 };
 
 export const StacksNetworks = {
-  'stacks:1': {
-    name: 'Mainnet',
-    network: mainnet,
-    loanContractAddress: '',
-    loanContractName: '',
-    managerContractAddress: '',
-    managerContractName: '',
-    assetContractAddress: '',
-    assetContractName: '',
-    assetName: '',
-    ioClientURI: 'api.mainnet.hiro.so',
-  },
-  'stacks:2147483648': {
     name: 'Testnet',
     network: testnet,
     loanContractAddress: process.env.REACT_APP_STACKS_TESTNET_CONTRACT_ADDRESS,
@@ -51,17 +20,4 @@ export const StacksNetworks = {
     assetContractName: process.env.REACT_APP_STACKS_ASSET_CONTRACT_NAME,
     assetName: process.env.REACT_APP_STACKS_ASSET_NAME,
     apiBase: 'api.testnet.hiro.so',
-  },
-  'stacks:42': {
-    name: 'Mocknet',
-    network: mocknet,
-    loanContractAddress: process.env.REACT_APP_STACKS_DEVNET_CONTRACT_ADDRESS,
-    loanContractName: process.env.REACT_APP_STACKS_SAMPLE_CONTRACT_NAME,
-    managerContractAddress: process.env.REACT_APP_STACKS_DEVNET_MANAGER_ADDRESS,
-    managerContractName: process.env.REACT_APP_STACKS_MANAGER_NAME,
-    assetContractAddress: process.env.REACT_APP_STACKS_DEVNET_MANAGER_ADDRESS,
-    assetContractName: process.env.REACT_APP_STACKS_ASSET_CONTRACT_NAME,
-    assetName: process.env.REACT_APP_STACKS_ASSET_NAME,
-    apiBase: 'dev-oracle.dlc.link',
-  },
-};
+  }
