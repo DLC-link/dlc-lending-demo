2fc8cf30ff474ee69fb55d84d6c6044749182086
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-09-15 13:45:42 +0200

    feat: modified env.template

--- src/networks/networks.js
+++ src/networks/networks.js
@@ -1,21 +1,31 @@
-import { StacksTestnet } from '@stacks/network';
+import { StacksMainnet, StacksTestnet, StacksMocknet } from '@stacks/network';
 
+const mainnet = new StacksMainnet();
 const testnet = new StacksTestnet();
+const mocknet = new StacksMocknet({
+  url: process.env.REACT_APP_STACKS_DEVNET_ADDRESS,
+});
+
+const stacksNetworks = {
+  'mainnet': mainnet,
+  'testnet': testnet,
+  'mocknet': mocknet,
+};
 
 export const EthereumNetwork = {
-    protocolContractAddress: process.env.REACT_APP_SEPOLIA_PROTOCOL_CONTRACT_ADDRESS,
-    usdcAddress: process.env.REACT_APP_SEPOLIA_USDC_ADDRESS,
-    dlcManagerAddress: process.env.REACT_APP_SEPOLIA_DLC_MANAGER_ADDRESS,
+    protocolContractAddress: process.env.REACT_APP_ETHEREUM_PROTOCOL_CONTRACT_ADDRESS,
+    usdcAddress: process.env.REACT_APP_ETHEREUM_USDC_ADDRESS,
+    dlcManagerAddress: process.env.REACT_APP_ETHEREUM_DLC_MANAGER_ADDRESS,
 };
 
 export const StacksNetwork = {
-    network: testnet,
-    loanContractAddress: process.env.REACT_APP_STACKS_TESTNET_CONTRACT_ADDRESS,
+    network: stacksNetworks[process.env.REACT_APP_STACKS_NETWORK],
+    loanContractAddress: process.env.REACT_APP_STACKS_CONTRACT_ADDRESS,
     loanContractName: process.env.REACT_APP_STACKS_SAMPLE_CONTRACT_NAME,
-    managerContractAddress: process.env.REACT_APP_STACKS_TESTNET_CONTRACT_ADDRESS,
+    managerContractAddress: process.env.REACT_APP_STACKS_CONTRACT_ADDRESS,
     managerContractName: process.env.REACT_APP_STACKS_MANAGER_NAME,
-    assetContractAddress: process.env.REACT_APP_STACKS_TESTNET_CONTRACT_ADDRESS,
+    assetContractAddress: process.env.REACT_APP_STACKS_CONTRACT_ADDRESS,
     assetContractName: process.env.REACT_APP_STACKS_ASSET_CONTRACT_NAME,
     assetName: process.env.REACT_APP_STACKS_ASSET_NAME,
-    apiBase: 'api.testnet.hiro.so',
+    apiBase: process.env.REACT_APP_STACKS_API_BASE_URL,
   }
